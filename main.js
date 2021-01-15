27be52911cb796af45fb91af906d1836683a03e6
AllCodeForever <77513729+AllCodeForever@users.noreply.github.com>
2021-01-15 22:10:12 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+var name = Math.floor(Math.random() * 3) + 1
+var no = Math.floor(Math.random() * 3) + 1
+var dates = Math.floor(Math.random() * 3) + 1
+if (name==1){
+ document.getElementById("name").innerHTML = "Jhon Davis"
+}
+if (name==2){
+ document.getElementById("name").innerHTML = "Mary Lockstone"
+}
+if (name==3){
+ document.getElementById("name").innerHTML = "Toilet Milk"
+}
+if (no==1){
+ document.getElementById("no").innerHTML = "12720"
+}
+if (no==2){
+ document.getElementById("no").innerHTML = "62811"
+}
+if (no==3){
+ document.getElementById("no").innerHTML = "72813"
+}
+if (dates==1){
+ document.getElementById("dates").innerHTML = "15/21"
+}
+if (dates==2){
+ document.getElementById("dates").innerHTML = "11/19"
+}
+If (dates==3){
+ document.getElementById("dates").innerHTML = "1/22"
+}
